875dafc31ed4bc8f29d5f0fe066848d62988b30f
bluefireoly <bluefireolymp@gmail.com>
2020-10-16 20:35:37 +0200

    New database data structure

--- src/main/kotlin/net/axay/hglaborlobby/database/DatabaseManager.kt
+++ src/main/kotlin/net/axay/hglaborlobby/database/DatabaseManager.kt
@@ -3,23 +3,17 @@ package net.axay.hglaborlobby.database
 import com.mongodb.client.model.IndexOptions
 import net.axay.blueutils.database.mongodb.MongoDB
 import net.axay.hglaborlobby.config.ConfigManager
+import net.axay.hglaborlobby.database.data.PlayerSettings
 import net.axay.hglaborlobby.main.PLUGIN_DATA_PREFIX
-import net.axay.kspigot.serialization.serializables.SerializableLocation
 import org.litote.kmongo.ascendingIndex
-import java.util.*
 
 object DatabaseManager {
 
     private val mongoDB = MongoDB(ConfigManager.databaseLoginInformation)
 
     val playerSettings
-            = mongoDB.getCollectionOrCreate<DatabasePlayerSettings>("${PLUGIN_DATA_PREFIX}player_settings") {
-                it.createIndex(ascendingIndex(DatabasePlayerSettings::uuid), IndexOptions().unique(true))
+            = mongoDB.getCollectionOrCreate<PlayerSettings>("${PLUGIN_DATA_PREFIX}player_settings") {
+                it.createIndex(ascendingIndex(PlayerSettings::uuid), IndexOptions().unique(true))
             }
 
 }
-
-data class DatabasePlayerSettings(
-        val uuid: UUID,
-        val customSpawnLoc: SerializableLocation
-)
---
+++ src/main/kotlin/net/axay/hglaborlobby/database/data/DatabasePlayerSettings.kt
@@ -0,0 +1,21 @@
+package net.axay.hglaborlobby.database.data
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import net.axay.kspigot.serialization.serializables.SerializableLocation
+import java.util.*
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class PlayerSettings(
+    val uuid: UUID,
+    val customSpawnLoc: SerializableLocation?,
+    val privacySettings: PlayerPrivacySettings
+) {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    data class PlayerPrivacySettings(
+        val ifCountry: Boolean,
+        val ifState: Boolean,
+        val ifCity: Boolean
+    )
+
+}
