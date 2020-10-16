abaa81a0d125487be5146f16f0c6ef75de11553a
bluefireoly <bluefireolymp@gmail.com>
2020-10-17 01:33:20 +0200

    Update DatabasePlayerSettings.kt

--- src/main/kotlin/net/axay/hglaborlobby/database/data/DatabasePlayerSettings.kt
+++ src/main/kotlin/net/axay/hglaborlobby/database/data/DatabasePlayerSettings.kt
@@ -8,7 +8,8 @@ import java.util.*
 data class PlayerSettings(
     val uuid: UUID,
     val customSpawnLoc: SerializableLocation? = null,
-    val privacySettings: PlayerPrivacySettings? = null
+    val privacySettings: PlayerPrivacySettings,
+    val ifSeeJoinMessages: Boolean = true
 ) {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -16,6 +17,8 @@ data class PlayerSettings(
         val ifCountry: Boolean,
         val ifState: Boolean,
         val ifCity: Boolean
-    )
+    ) {
+        val ifLoadAny get() = ifCountry || ifState || ifCity
+    }
 
 }
