a74da4de52cd7448de087870364dd6dd2fdfd85c
bluefireoly <bluefireolymp@gmail.com>
2020-10-27 22:47:47 +0100

    Added ServerProtection

---
+++ src/main/kotlin/net/axay/hglaborlobby/protection/ServerProtection.kt
@@ -0,0 +1,66 @@
+package net.axay.hglaborlobby.protection
+
+import net.axay.kspigot.event.listen
+import org.bukkit.entity.Player
+import org.bukkit.entity.Projectile
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerInteractEntityEvent
+import org.bukkit.event.player.PlayerInteractEvent
+
+object ServerProtection {
+
+    fun enable() {
+
+        listen<PlayerInteractEvent> {
+            GeneralProtectionUtils.checkPlayerAction(it)
+        }
+
+        listen<PlayerInteractEntityEvent> {
+            GeneralProtectionUtils.checkPlayerAction(it)
+        }
+
+        listen<EntityDamageEvent> {
+            if (it is Player)
+                it.isCancelled = true
+        }
+
+        listen<EntityDamageByEntityEvent> {
+
+            val damager = it.damager
+
+            if (damager is Player)
+                GeneralProtectionUtils.checkPlayerAction(it, damager)
+
+            else if (damager is Projectile) {
+                val source = damager.shooter
+                if (source is Player)
+                    GeneralProtectionUtils.checkPlayerAction(it, source)
+            }
+
+        }
+
+        listen<InventoryClickEvent> {
+
+            val whoClicked = it.whoClicked
+
+            if (whoClicked is Player)
+                if (it.inventory == whoClicked.inventory)
+                    GeneralProtectionUtils.checkPlayerAction(it, whoClicked)
+
+        }
+
+        listen<PlayerDeathEvent> {
+            it.drops.clear()
+        }
+
+        listen<PlayerDropItemEvent> {
+            GeneralProtectionUtils.checkPlayerAction(it)
+        }
+
+    }
+
+}
