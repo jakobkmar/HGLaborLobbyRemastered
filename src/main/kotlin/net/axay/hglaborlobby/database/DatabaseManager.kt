f874635d1372201178aa1d5627a8ef19aebb28d5
bluefireoly <bluefireolymp@gmail.com>
2020-10-27 21:12:13 +0100

    Update DatabaseManager.kt

--- src/main/kotlin/net/axay/hglaborlobby/database/DatabaseManager.kt
+++ src/main/kotlin/net/axay/hglaborlobby/database/DatabaseManager.kt
@@ -3,7 +3,7 @@ package net.axay.hglaborlobby.database
 import com.mongodb.client.model.IndexOptions
 import net.axay.blueutils.database.mongodb.MongoDB
 import net.axay.hglaborlobby.config.ConfigManager
-import net.axay.hglaborlobby.database.data.PlayerSettings
+import net.axay.hglaborlobby.data.PlayerSettings
 import net.axay.hglaborlobby.main.PLUGIN_DATA_PREFIX
 import org.litote.kmongo.ascendingIndex
 
