932e1fe7b4d073da79d516f4e0074ee9b6074f63
bluefireoly <bluefireolymp@gmail.com>
2020-10-27 21:12:45 +0100

    Added ipServiceConfig to ConfigManager

--- src/main/kotlin/net/axay/hglaborlobby/config/ConfigManager.kt
+++ src/main/kotlin/net/axay/hglaborlobby/config/ConfigManager.kt
@@ -1,13 +1,20 @@
 package net.axay.hglaborlobby.config
 
 import net.axay.blueutils.database.DatabaseLoginInformation
+import net.axay.hglaborlobby.data.IPServiceConfig
 import net.axay.kspigot.config.PluginFile
 import net.axay.kspigot.config.kSpigotJsonConfig
 
 object ConfigManager {
 
     val databaseLoginInformation
-            by kSpigotJsonConfig(PluginFile("databaseLoginInformation.json"))
-                { DatabaseLoginInformation.NOTSET_DEFAULT }
+            by kSpigotJsonConfig(PluginFile("databaseLoginInformation.json")) {
+                DatabaseLoginInformation.NOTSET_DEFAULT
+            }
+
+    val ipServiceConfig
+            by kSpigotJsonConfig(PluginFile("ipServiceConfig.json")) {
+                IPServiceConfig.NOTSET_DEFAULT
+            }
 
 }
