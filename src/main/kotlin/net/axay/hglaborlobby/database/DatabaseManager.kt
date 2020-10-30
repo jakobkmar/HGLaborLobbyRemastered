3a28a451adc5c5770e7765e5a42b91bc05cb39ee
bluefireoly <bluefireolymp@gmail.com>
2020-10-30 22:37:22 +0100

    Update DatabaseManager.kt

--- src/main/kotlin/net/axay/hglaborlobby/database/DatabaseManager.kt
+++ src/main/kotlin/net/axay/hglaborlobby/database/DatabaseManager.kt
@@ -3,13 +3,15 @@ package net.axay.hglaborlobby.database
 import net.axay.blueutils.database.mongodb.MongoDB
 import net.axay.hglaborlobby.config.ConfigManager
 import net.axay.hglaborlobby.data.PlayerSettings
+import net.axay.hglaborlobby.data.Warp
 import net.axay.hglaborlobby.main.PLUGIN_DATA_PREFIX
 
 object DatabaseManager {
 
     private val mongoDB = MongoDB(ConfigManager.databaseLoginInformation, spigot = true)
 
-    val playerSettings
-            = mongoDB.getCollectionOrCreate<PlayerSettings>("${PLUGIN_DATA_PREFIX}player_settings")
+    val playerSettings = mongoDB.getCollectionOrCreate<PlayerSettings>("${PLUGIN_DATA_PREFIX}player_settings")
+
+    val warps = mongoDB.getCollectionOrCreate<Warp>("${PLUGIN_DATA_PREFIX}warps")
 
 }
