ddf112ef736df5b430f5fdca7094bb0abb1f96db
bluefireoly <bluefireolymp@gmail.com>
2021-02-02 16:36:00 +0100

    Improve ElytraLauncher

--- src/main/kotlin/net/axay/hglaborlobby/pads/ElytraLauncher.kt
+++ src/main/kotlin/net/axay/hglaborlobby/pads/ElytraLauncher.kt
@@ -1,37 +1,43 @@
 package net.axay.hglaborlobby.pads
 
+import net.axay.hglaborlobby.functionality.makeLobbyItem
 import net.axay.kspigot.event.listen
+import net.axay.kspigot.extensions.bukkit.isGroundSolid
+import net.axay.kspigot.sound.sound
 import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
-import org.bukkit.util.Vector
 
 object ElytraLauncher {
 
     fun enable() {
 
         listen<PlayerInteractEvent>() {
-            if (it.clickedBlock?.type == Material.BIRCH_PRESSURE_PLATE &&
+
+            if (
+                it.action == Action.PHYSICAL &&
+                it.clickedBlock?.type == Material.BIRCH_PRESSURE_PLATE &&
                 it.clickedBlock?.location?.subtract(0.0, 1.0, 0.0)?.block?.type == Material.STRIPPED_JUNGLE_WOOD
             ) {
-                it.player.velocity = Vector(0, 4, 0)
+                it.player.velocity = it.player.velocity.setY(10)
+
                 it.player.inventory.chestplate = ItemStack(Material.ELYTRA)
-                it.player.inventory.addItem(ItemStack(Material.FEATHER))
+                it.player.inventory.addItem(ItemStack(Material.FEATHER).makeLobbyItem())
+
+                it.player.sound(Sound.ITEM_ARMOR_EQUIP_ELYTRA)
             }
 
-            if (it.player.inventory.itemInMainHand.type == Material.FEATHER &&
+            else if (
+                it.material == Material.FEATHER &&
                 it.player.isGliding &&
-                it.action == Action.RIGHT_CLICK_AIR
+                !it.player.isGroundSolid
             ) {
                 it.player.velocity = it.player.location.direction.multiply(1.5)
             }
 
         }
 
-
-
     }
 }
-
-
