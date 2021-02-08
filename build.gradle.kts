79b7e0597f3004d016d5213f441e10848430fb99
BestAutoyes <enrico-eifert@t-online.de>
2021-02-08 19:11:50 +0100

    Changed KSpigot version to 1.16.5R_23

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     compileOnly("org.spigotmc", "spigot", "1.16.5-R0.1-SNAPSHOT")
 
     // KSPIGOT
-    implementation("net.axay", "KSpigot", "v1.16.4_R22")
+    implementation("net.axay", "KSpigot", "v1.16.5_R23")
 
     // BLUEUTILS
     compileOnly("net.axay", "BlueUtils", "1.0.2")
