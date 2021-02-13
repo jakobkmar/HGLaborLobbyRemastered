dc5d3b96cb0d4d60ba6220a535f9889bdb622697
BestAutoyes <enrico-eifert@t-online.de>
2021-02-13 11:30:48 +0100

    changed getter/setter

--- src/main/kotlin/net/axay/hglaborlobby/hgqueue/HGInfo.kt
+++ src/main/kotlin/net/axay/hglaborlobby/hgqueue/HGInfo.kt
@@ -12,63 +12,58 @@ class HGInfo {
     companion object {
         var infos = hashMapOf<String, HGInfo>()
     }
-    private var serverName: String? = null
-    private var serverPort = 0
-    private var maxPlayers = 0
-    private var onlinePlayers = 0
-    private var timeInSeconds = 0
-    private var gameState: String? = null
+
+    var serverName: String? = null
+        get() {
+            return field
+        }
+        set(serverName) {
+            field = serverName
+        }
+
+    var serverPort = 0
+        get() {
+            return field
+        }
+        set(serverPort) {
+            field = serverPort
+        }
+
+    var maxPlayers = 0
+        get() {
+            return field
+        }
+        set(maxPlayers) {
+            field = maxPlayers
+        }
+
+    var onlinePlayers = 0
+        get() {
+            return field
+        }
+        set(onlinePlayers) {
+            field = onlinePlayers
+        }
+
+    var timeInSeconds = 0
+        get() {
+            return field
+        }
+        set(timeInSeconds) {
+            field = timeInSeconds
+        }
+
+    var gameState: String? = null
+        get() {
+            return field
+        }
+        set(gameState) {
+            field = gameState
+        }
+
     var item: ItemStack? = null
 
-    fun getServerName(): String? {
-        return serverName
-    }
-
-    fun setServerName(serverName: String?) {
-        this.serverName = serverName
-    }
-
-    fun getServerPort(): Int {
-        return serverPort
-    }
-
-    fun setServerPort(serverPort: Int) {
-        this.serverPort = serverPort
-    }
-
-    fun getGameState(): String? {
-        return gameState
-    }
-
-    fun setGameState(gameState: String?) {
-        this.gameState = gameState
-    }
-
-    fun getMaxPlayers(): Int {
-        return maxPlayers
-    }
-
-    fun setMaxPlayers(maxPlayers: Int) {
-        this.maxPlayers = maxPlayers
-    }
-
-    fun getOnlinePlayers(): Int {
-        return onlinePlayers
-    }
-
-    fun setOnlinePlayers(onlinePlayers: Int) {
-        this.onlinePlayers = onlinePlayers
-    }
-
-    fun getTimeInSeconds(): Int {
-        return timeInSeconds
-    }
-
-    fun setTimeInSeconds(timeInSeconds: Int) {
-        this.timeInSeconds = timeInSeconds
-    }
-
-    fun gameState() = GameState.valueOf(getGameState()!!)
+    fun gameState() = GameState.valueOf(gameState!!)
 
     fun formattedTime(): String {
         val min: Int = timeInSeconds / 60
@@ -80,16 +75,20 @@ class HGInfo {
 fun HGInfo.getNewItem() = itemStack(gameState().material) {
     amount = 1
     meta {
-        name = "${KColors.CORNSILK}${getServerName()}"
+        name = "${KColors.CORNSILK}${serverName}"
         addLore {
-            +"${KColors.GRAY}State ${KColors.DARKGRAY}» ${gameState().name()}"
-            +"${KColors.GRAY}Players ${KColors.DARKGRAY}» ${KColors.DEEPSKYBLUE}${getOnlinePlayers()}"
+            +"${KColors.GRAY}State ${KColors.DARKGRAY}»${gameState().color} ${gameState().name()}"
+            +"${KColors.GRAY}Players ${KColors.DARKGRAY}» ${gameState().color}${onlinePlayers}"
             if (gameState() == GameState.LOBBY) +"${KColors.GRAY}Starting in ${KColors.DARKGRAY}» ${formattedTime()}"
             else +"${KColors.GRAY}Time ${KColors.DARKGRAY}» ${formattedTime()}"
-
             +"${KColors.DARKGRAY}${KColors.STRIKETHROUGH}                      "
-            +"${KColors.LIGHTSKYBLUE}Left ${KColors.GRAY}click to join ${KColors.UNDERLINE}server"
-            +"${KColors.MEDIUMPURPLE}Right ${KColors.GRAY}click to join ${KColors.UNDERLINE}queue"
+
+            if (onlinePlayers < maxPlayers) {
+                if (gameState() != GameState.INGAME && gameState() != GameState.END)
+                    +"${KColors.LIGHTSKYBLUE}Left ${KColors.GRAY}click to join ${KColors.UNDERLINE}server"
+                if (gameState() == GameState.END || gameState() == GameState.LOBBY)
+                    +"${KColors.MEDIUMPURPLE}Right ${KColors.GRAY}click to join ${KColors.UNDERLINE}queue"
+            }
         }
     }
 }
