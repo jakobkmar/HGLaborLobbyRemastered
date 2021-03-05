b7cfaf6b484c31f0aa0318c0d51de21718e0114d
BestAutoyes <enrico-eifert@t-online.de>
2021-03-06 00:14:35 +0100

    changed kspigot version again xd

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     compileOnly("org.spigotmc", "spigot", "1.16.5-R0.1-SNAPSHOT")
 
     // KSPIGOT
-    implementation("net.axay", "KSpigot", "v1.16.5_R24")
+    implementation("net.axay:kspigot:1.16.24")
 
     // BLUEUTILS
     compileOnly("net.axay", "BlueUtils", "1.0.2")
