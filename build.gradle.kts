1651fbe497fd0750034acc324dd6c3fc668bf5a4
Jakob K <bluefireolymp@gmail.com>
2021-04-07 16:20:14 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,84 +1,44 @@
-@file:Suppress("PropertyName")
-
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-/*
- * BUILD CONSTANTS
- */
-
-val JVM_VERSION = JavaVersion.VERSION_11
-val JVM_VERSION_STRING = JVM_VERSION.versionString
-
-/*
- * PROJECT
- */
+val jvmVersion = JavaVersion.VERSION_11
+val jvmVersionString = jvmVersion.majorVersion.let { if (it.toInt() <= 10) "1.$it" else it }
 
 group = "net.axay"
 version = "1.0.0"
 
-/*
- * PLUGINS
- */
-
 plugins {
-
     kotlin("jvm") version "1.4.32"
 
     id("com.github.johnrengelman.shadow") version "6.1.0"
-
     kotlin("plugin.serialization") version "1.4.21"
-
 }
 
-/*
- * DEPENDENCY MANAGEMENT
- */
-
 repositories {
-    jcenter()
     mavenLocal()
+
+    jcenter()
     mavenCentral()
-    // AnvilGUI
     maven("https://repo.codemc.io/repository/maven-snapshots/")
 }
 
 dependencies {
-
-    // SPIGOT
     compileOnly("org.spigotmc", "spigot", "1.16.5-R0.1-SNAPSHOT")
-
-    // KSPIGOT
     implementation("net.axay:kspigot:1.16.26")
-
-    // BLUEUTILS
     compileOnly("net.axay", "BlueUtils", "1.0.2")
-
-    // KMONGO and MONGODB
     compileOnly("org.litote.kmongo", "kmongo-core", "4.2.3")
     compileOnly("org.litote.kmongo", "kmongo-serialization-mapping", "4.2.3")
-
 }
 
-/*
- * BUILD
- */
-
-// JVM VERSION
-
-java.sourceCompatibility = JVM_VERSION
-java.targetCompatibility = JVM_VERSION
+java.sourceCompatibility = jvmVersion
+java.targetCompatibility = jvmVersion
 
 tasks.withType<KotlinCompile> {
-    configureJvmVersion()
-    configureJvmVersion()
+    kotlinOptions.jvmTarget = jvmVersionString
 }
-
-// SHADOW
 
 tasks {
     shadowJar {
-
         dependencies {
             exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib.*"))
         }
@@ -86,22 +46,7 @@ tasks {
         minimize()
 
         simpleRelocate("net.axay.kspigot")
-
     }
-}
-
-/*
- * EXTENSIONS
- */
-
-val JavaVersion.versionString
-    get() = majorVersion.let {
-        val version = it.toInt()
-        if (version <= 10) "1.$it" else it
-    }
-
-fun KotlinCompile.configureJvmVersion() {
-    kotlinOptions.jvmTarget = JVM_VERSION_STRING
 }
 
 fun ShadowJar.simpleRelocate(pattern: String) {
