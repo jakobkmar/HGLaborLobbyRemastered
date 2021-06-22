f0a477a1374261c1b66f13a69444b590b350fa67
mooziii <mooz@moozunity.de>
2021-06-22 15:19:32 +0200

    Remove empty lines

--- src/main/kotlin/net/axay/hglaborlobby/gui/guis/WarpGUI.kt
+++ src/main/kotlin/net/axay/hglaborlobby/gui/guis/WarpGUI.kt
@@ -13,7 +13,6 @@ import net.axay.kspigot.items.itemStack
 import net.axay.kspigot.items.name
 import net.axay.kspigot.items.setMeta
 import net.axay.kspigot.items.toLoreList
-import net.axay.kspigot.runnables.async
 import net.axay.kspigot.runnables.firstAsync
 import net.axay.kspigot.runnables.thenSync
 import net.axay.kspigot.utils.hasMark
@@ -24,18 +23,12 @@ import org.bukkit.inventory.ItemStack
 object WarpGUI {
 
     private val warpsGUI = kSpigotGUI(GUIType.THREE_BY_NINE) {
-
         title = "${KColors.BLACK}WARPS"
-
         page(1) {
-
             placeholder(Slots.Border, ItemStack(Material.WHITE_STAINED_GLASS_PANE))
-
             val warpsCompound = createRectCompound<Warp>(
-
                 Slots.RowTwoSlotTwo,
                 Slots.RowTwoSlotEight,
-
                 iconGenerator = {
                     itemStack(it.icon) {
                         setMeta {
@@ -48,49 +41,32 @@ object WarpGUI {
                         }
                     }
                 },
-
                 onClick = { clickEvent, element ->
-
                     clickEvent.bukkitEvent.isCancelled = true
-
                     clickEvent.bukkitEvent.whoClicked.teleport(element.location)
-
                 }
-
             )
-
             firstAsync {
                 WarpsHolder.instance.warps
             }.thenSync {
                 warpsCompound.addContent(it)
             }.execute()
-
         }
-
     }
 
     fun enable() {
-
-
         MainGUI.addContent(MainGUI.MainGUICompoundElement(
             Material.COMPASS,
             "Warps",
             "Sehe alle Warps, die existieren.",
             onClick = { it.player.openGUI(warpsGUI) }
         ))
-
         listen<PlayerInteractEvent> {
-
             if (!it.action.isRightClick) return@listen
-
             if (it.item?.hasMark("warps") == true) {
                 it.isCancelled = true
                 it.player.openGUI(warpsGUI)
             }
-
         }
-
-
     }
-
 }
